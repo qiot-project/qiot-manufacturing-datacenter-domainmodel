6b3fa191eef71561887d8fdd5b1acec75bebfd91
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-09-18 11:19:21 +0200

    telemetry bugfix

--- src/main/java/io/qiot/manufacturing/datacenter/commons/domain/telemetry/production/StageProductionValidationTelemetryDTO.java
+++ src/main/java/io/qiot/manufacturing/datacenter/commons/domain/telemetry/production/StageProductionValidationTelemetryDTO.java
@@ -3,6 +3,8 @@
  */
 package io.qiot.manufacturing.datacenter.commons.domain.telemetry.production;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.influxdb.annotations.Column;
 import com.influxdb.annotations.Measurement;
 
 import io.qiot.manufacturing.all.commons.domain.production.ProductionChainStageEnum;
@@ -18,8 +20,14 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 public class StageProductionValidationTelemetryDTO
         extends AbstractProductionValidationTelemetryDTO {
 
+    @JsonProperty(value = "item_id")
+    @Column(name = "item_id", tag = true)
     public int itemId;
+    @JsonProperty(value = "stage_id")
+    @Column(name = "stage_id", tag = true)
     public ProductionChainStageEnum stage;
+    @JsonProperty(value = "product_line_id")
+    @Column(name = "success")
     public boolean success;
     
 
