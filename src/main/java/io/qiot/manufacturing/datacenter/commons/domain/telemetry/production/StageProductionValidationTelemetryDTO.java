73942a9c8928d16521e01e35c69306473bc9fb78
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-09-19 13:37:34 +0200

    Fix attribute names

--- src/main/java/io/qiot/manufacturing/datacenter/commons/domain/telemetry/production/StageProductionValidationTelemetryDTO.java
+++ src/main/java/io/qiot/manufacturing/datacenter/commons/domain/telemetry/production/StageProductionValidationTelemetryDTO.java
@@ -4,6 +4,7 @@
 package io.qiot.manufacturing.datacenter.commons.domain.telemetry.production;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.influxdb.annotations.Column;
 import com.influxdb.annotations.Measurement;
 
@@ -17,6 +18,8 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
  */
 @Measurement(name = "production_stages")
 @RegisterForReflection
+@JsonPropertyOrder(value = { "instant", "factory_id", "machinery_id",
+        "product_line_id", "item_id", "stage_id", "success" })
 public class StageProductionValidationTelemetryDTO
         extends AbstractProductionValidationTelemetryDTO {
 
@@ -26,9 +29,29 @@ public class StageProductionValidationTelemetryDTO
     @JsonProperty(value = "stage_id")
     @Column(name = "stage_id", tag = true)
     public ProductionChainStageEnum stage;
-    @JsonProperty(value = "product_line_id")
+    @JsonProperty(value = "success")
     @Column(name = "success")
     public boolean success;
-    
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("StageProductionValidationTelemetryDTO [productLineId=");
+        builder.append(productLineId);
+        builder.append(", machineryId=");
+        builder.append(machineryId);
+        builder.append(", factoryId=");
+        builder.append(factoryId);
+        builder.append(", time=");
+        builder.append(time);
+        builder.append(", itemId=");
+        builder.append(itemId);
+        builder.append(", stage=");
+        builder.append(stage);
+        builder.append(", success=");
+        builder.append(success);
+        builder.append("]");
+        return builder.toString();
+    }
 
 }
