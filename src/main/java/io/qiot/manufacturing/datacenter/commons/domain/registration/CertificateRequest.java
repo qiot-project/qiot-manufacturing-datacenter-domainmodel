d33a55eab4d930856716bd8fe52a813494ee50f1
Andrea Battaglia <andreabattaglia@users.noreply.github.com>
2021-10-17 10:03:57 +0200

    Fix "isCA" attr name; add javadoc

--- src/main/java/io/qiot/manufacturing/datacenter/commons/domain/registration/CertificateRequest.java
+++ src/main/java/io/qiot/manufacturing/datacenter/commons/domain/registration/CertificateRequest.java
@@ -14,29 +14,54 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
  * @author mmascia
  * @author andreabattaglia
  */
+/**
+ * @author andreabattaglia
+ *
+ */
 @JsonPropertyOrder(value = { "id", "name", "domain", "serial",
-        "keyStorePassword", "ac" })
+        "keyStorePassword", "ca" })
 @RegisterForReflection
 public class CertificateRequest {
 
+    /**
+     * 
+     */
     @JsonProperty(value = "id")
     @NotNull
     public UUID id;
+    /**
+    * 
+    */
     @JsonProperty(value = "name")
     @NotNull
     @Pattern(regexp = "[a-z0-9]([-a-z0-9]*[a-z0-9])?(\\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*")
     public String name;
+    /**
+     * <ul>
+     * <li><b>Factory</b>: domain = ""</li>
+     * <li><b>Machinery</b>: domain = "." + factoryId</li>
+     * </ul>
+     */
     @JsonProperty(value = "domain")
     @NotNull
     public String domain;
+    /**
+     * 
+     */
     @JsonProperty(value = "serial")
     @NotNull
     public String serial;
+    /**
+     * 
+     */
     @JsonProperty(value = "keyStorePassword")
     @NotNull
     public String keyStorePassword;
-    @JsonProperty(value = "ac", defaultValue = "false")
+    /**
+     * 
+     */
+    @JsonProperty(value = "ca", defaultValue = "false")
     @NotNull
-    public boolean ac;
+    public boolean ca;
 
 }
